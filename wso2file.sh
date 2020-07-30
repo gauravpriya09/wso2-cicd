f7ab420c4b269f4a5cb89f4675d59934954c7324
gauravpriya09 <64337755+gauravpriya09@users.noreply.github.com>
2020-07-30 18:06:50 +0530

    Update wso2file.sh

--- wso2file.sh
+++ wso2file.sh
@@ -1,3 +1,30 @@
-echo "Hello WSO2 Manager"
-echo "testing webhooks"
-echo "Telifonica"
+#!/bin/bash
+user=`echo $1`
+paswd=`echo $2`
+echo $user $paswd
+/var/lib/jenkins/workspace/wso2_freestyle/tool/apictl/apictl login production -u ${user} -p ${paswd} -k
+IFS='
+'
+if [ "X$3" == "Xexport" ]
+then
+for i in `cat api.config`
+do
+echo $i
+aname=`echo $i|awk -F":" '{print $1}'`
+version=`echo $i|awk -F":" '{print $2}'`
+echo ${aname} ${version}
+/var/lib/jenkins/workspace/wso2_freestyle/tool/apictl/apictl export-api -n ${aname} -v ${version} -e production -k
+done
+fi
+
+
+if [ "X$3" == "Ximport" ]
+then
+for i in `cat api.config`
+do
+aname=`echo $i|awk -F":" '{print $1}'`
+version=`echo $i|awk -F":" '{print $2}'`
+echo ${aname} ${version}
+/var/lib/jenkins/workspace/wso2_freestyle/tool/apictl/apictl import-api -f ./src/${aname}/${version}/${aname}_${version}.zip -e production -k
+done
+fi
